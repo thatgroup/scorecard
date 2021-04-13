15397aadb7db03b141486468dfc59cf1ef94165a
charliematters <charliematters@gmail.com>
2021-04-13 14:16:21 +0100

    Updated deps

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   env: {
     // The THEME variable needs to be available on the client and server side, and this is the Next.JS way of doing that
     THEME: process.env.THEME,
